d855158e06db4970d4f873f39864d830d9fc6c7a
Eduardo-Hannah <eduardo.huarcaya@hannahlab.com>
2025-12-19 15:22:01 -0500

    Migrando proyecto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Button>Click aqui</Button>
+    </div>
+  );
+}
+
+export default App;
